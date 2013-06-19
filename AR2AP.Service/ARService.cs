1912342dbeefeac6b05352420d962e216e2a6d30
sun wu qiang <sunwuqiang@gmail.com>
2013-06-20 03:19:32 +0800

    add 2 AR pages

---
+++ AR2AP.Service/ARService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AR2AP.BLL;
+using AR2AP.BLL.Repository;
+using AR2AP.Utility;
+
+namespace AR2AP.Service
+{
+    public class ARService
+    {
+        public void Add(AREntity entity)
+        {
+            using (RepositoryTransaction trans = new RepositoryTransaction())
+            {
+                var repository = trans.GetARRepository();
+                repository.Add(entity);
+                trans.Commit();
+            }
+        }
+        public IList<AREntity> GetEntities()
+        {
+            using (RepositoryTransaction trans = new RepositoryTransaction())
+            {
+                var repository = trans.GetARRepository();
+                return repository.FindBySpecification(o => true).ToList();
+            }
+        }
+    }
+}
