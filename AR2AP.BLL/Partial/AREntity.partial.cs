9d57728e5c66441f437abf09d8306fcf2ffb753a
sun wu qiang <sunwuqiang@gmail.com>
2013-06-27 00:34:04 +0800

    Add DateTime EditorTemplate

--- AR2AP.BLL/Partial/AREntity.partial.cs
+++ AR2AP.BLL/Partial/AREntity.partial.cs
@@ -31,23 +31,27 @@ namespace AR2AP.BLL
             public string ContractNo { get; set; }
             [Display(Name = "Campaingn")]
             public string Campaingn { get; set; }
-            [Display(Name = "Compaign Start")]
+            [Display(Name = "Compaign Start")][UIHint("DateTime")]
             public string CompaignStart { get; set; }
             [Display(Name = "Compaign End")]
+            [UIHint("DateTime")]
             public string CompaignEnd { get; set; }
             [Display(Name = "Compaign Amount")]
             public string CompaignAmount { get; set; }
             [Display(Name = "Due Date")]
+            [UIHint("DateTime")]
             public string DueDate { get; set; }
             [Display(Name = "Invoice No")]
             public string InvoiceNo { get; set; }
             [Display(Name = "Invoice Date")]
+            [UIHint("DateTime")]
             public string InvoiceDate { get; set; }
             [Display(Name = "Invoice Type")]
             public byte InvoiceType { get; set; }
             [Display(Name = "Invoice Amount")]
             public string InvoiceAmount { get; set; }
             [Display(Name = "Revenue Confirmation Date")]
+            [UIHint("DateTime")]
             public string RevenueConfirmationDate { get; set; }
             [Display(Name = "Remark")]
             public string Remark { get; set; }
